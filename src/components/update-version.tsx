27a24620d1b2d4161c79b942842bcd06162c0bd9
saturn <saturn@codexcircle.it>
2025-12-24 05:22:52 +0100

    Update update-version.tsx

---
+++ src/components/update-version.tsx
@@ -0,0 +1,11 @@
+import { Card, CardDescription, CardTitle } from "./ui/card";
+
+const updateVersion = () => {
+  return (
+    <Card>
+      <CardTitle>Update Version</CardTitle>
+      <CardDescription>Update version available</CardDescription>
+    </Card>
+  );
+};
+export default updateVersion;
