4710193967ccd65dd036afdb778e2eb99a353bcb
saturn <saturn@codexcircle.it>
2026-01-26 03:00:16 +0100

    Update getStatusIcon.tsx

--- src/lib/getStatusIcon.tsx
+++ src/lib/getStatusIcon.tsx
@@ -1,10 +1,8 @@
 import type { QueueItemType } from "@/types/QueueItemType";
-import { AlertCircle, CheckCircle, Clock, Download } from "lucide-react";
+import { AlertCircle, CheckCircle, Clock, FileQuestion } from "lucide-react";
 
 export const getStatusIcon = (status: QueueItemType["status"]) => {
   switch (status) {
-    case "downloading":
-      return <Download className="h-4 w-4 animate-pulse text-blue-500" />;
     case "completed":
       return <CheckCircle className="h-4 w-4 text-green-500" />;
     case "error":
@@ -12,6 +10,6 @@ export const getStatusIcon = (status: QueueItemType["status"]) => {
     case "pending":
       return <Clock className="h-4 w-4 text-gray-500" />;
     default:
-      return <Clock className="h-4 w-4 text-gray-500" />;
+      return <FileQuestion className="h-4 w-4 text-gray-500" />;
   }
 };
